1bf377ff712b1b4c7951001f435baa06cd8ffd8f
Ariane de Almeida Franchetto <nanifranchetto@gmail.com>
2022-10-25 19:25:20 -0300

    Initial commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,37 @@
+<template>
+<footer id="footer">
+<p>
+
+    Make your burguer &copy; 2022.
+
+</p>    
+
+
+</footer>
+
+</template>
+
+
+<script>
+export default {
+name: "Footer"
+}
+
+
+</script>
+
+<style scoped>
+#footer {
+    height: 100px;
+    background-color:#111;
+    color: #FCBA03;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+
+}
+
+
+
+</style>
---
+++ src/views/Pedidos.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class="main-container">Pedidos</h1>
+  </div>
+</template>
